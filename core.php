d540152b658b9d94fd199851d7c9c4221f049d24
Josh Pollock <jpollock412@gmail.com>
2015-07-15 19:02:28 -0400

    init function

--- core.php
+++ core.php
@@ -4,7 +4,7 @@
  * Plugin URI:  
  * Description: Create custom fields with powerful conditionals and processors using a Caldera Forms as the metabox designer.
  * Version:     2.0.0-b-1
- * Author:      David Cramer
+ * Author:      David Cramer for CalderaWP LLC
  * Author URI:  
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -14,3 +14,13 @@ define( 'CCF_PATH',  plugin_dir_path( __FILE__ ) );
 define( 'CCF_URL',  plugin_dir_url( __FILE__ ) );
 define( 'CCF_VER', '2.0.0-b-1' );
 define( 'CCF_CORE',  __FILE__ );
+
+
+/**
+ * Load plugin
+ */
+add_action( 'init', 'cf_custom_fields_init' );
+function cf_custom_fields_init() {
+	include_once( CCF_PATH . '/includes/metabox.php' );
+	include_once( CCF_PATH . '/includes/to-post-type.php' );
+}
