CalderaWP/caldera-custom-fields

commit d09ce88984c0fa7c2f694209f80698c88b6e4762

add filters before saving meta fields

includes/metabox.php
@@ -132,7 +132,19 @@ function cf_custom_fields_save_meta_data($config, $form){
 		if(empty($form['fields'][$key])){
 			continue;
 		}
-		update_post_meta( $post->ID, $form['fields'][$key]['slug'], $value );
+
+		$slug = $form['fields'][$field]['slug'];
+
+		/**
+		 * Filter value before saving using to metabox processor
+		 * @since 2.0.3
+		 *
+		 * @param mixed $value The value to be saved
+		 * @param string $slug Slug of field
+		 * @param int $post_id ID of post
+		 */
+		$value = apply_filters( 'cf_custom_fields_pre_save_meta_key_metabox', $value, $slug, $post->ID );
+		update_post_meta( $post->ID, $slug, $value );
 		if(isset($field_toremove[$form['fields'][$key]['slug']])){
 			unset($field_toremove[$form['fields'][$key]['slug']]);
 		}

includes/to-post-type.php
@@ -346,7 +346,20 @@ function cf_custom_fields_capture_entry($config, $form){
 				cf_custom_fields_attach_file( $file , $entry_id );
 			}
 		}
-		update_post_meta( $entry_id, $form['fields'][$field]['slug'], $value );
+
+		$slug = $form['fields'][$field]['slug'];
+
+		/**
+		 * Filter value before saving using to post type processor
+		 *
+		 * @since 2.0.3
+		 *
+		 * @param mixed $value The value to be saved
+		 * @param string $slug Slug of field
+		 * @param int $entry ID of post
+		 */
+		$value = apply_filters( 'cf_custom_fields_pre_save_meta_key_to_post_type', $value, $slug, $entry_id );
+		update_post_meta( $entry_id, $slug, $value );
 	}
 
 	return array('Post ID' => $entry_id, 'ID' => $entry_id, 'permalink' => get_permalink( $entry_id ) );

commit f9d69c8af5f7d6f58ce6b7ab052728df1ebd892d

fixing bug introduced in d09ce88

includes/metabox.php
@@ -133,7 +133,7 @@ function cf_custom_fields_save_meta_data($config, $form){
 			continue;
 		}
 
-		$slug = $form['fields'][$field]['slug'];
+		$slug = $form['fields'][$key]['slug'];
 
 		/**
 		 * Filter value before saving using to metabox processor