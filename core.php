dfbc1cd7c41258fef3807bca7d3ed4f44cc1b292
Josh Pollock <jpollock412@gmail.com>
2016-06-20 19:04:31 -0400

    version 2.1.0

--- core.php
+++ core.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * Plugin Name: Caldera Custom Fields
- * Plugin URI:  
+ * Plugin URI:  https://CalderaWP.com/downloads/caldera-form-custom-fields
  * Description: Create custom fields with powerful conditionals and processors using a Caldera Forms as the metabox designer.
- * Version:     2.1.0-b1
+ * Version:     2.1.0
  * Author:      David Cramer for CalderaWP LLC
- * Author URI:  
+ * Author URI:  https://CalderaWP.com
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 define( 'CCF_PATH',  plugin_dir_path( __FILE__ ) );
 define( 'CCF_URL',  plugin_dir_url( __FILE__ ) );
-define( 'CCF_VER', '2.1.0-b1' );
+define( 'CCF_VER', '2.1.' );
 define( 'CCF_CORE',  __FILE__ );
 
 
